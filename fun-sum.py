1fdac47fe9413cb6bde0fbefd485994c326b48c4
Vinay Garge <vinay.garge@sbdinc.com>
2026-09-03 12:11:40 +0530

    PY-03092026

---
+++ fun-sum.py
@@ -0,0 +1,6 @@
+def add(number1:float, number2:float) -> float:
+    print(f'Adding {number1} + {number2}')
+    return number1+number2
+
+print(f'Sum is {add(number1=3,number2=5)}')
+print(f'Sum is {add(3.4,5.9)}')
