1364ed8ee003526ba4920f0d7fc3a7bef655a328
Vinay Garge <vgarge@gmail.com>
2026-09-06 09:01:29 +0530

    06092026

--- strings_ex.py
+++ strings_ex.py
@@ -1,6 +1,5 @@
 my_string = "This is a test string"
 
-print(f"Is following string alpha-numeric --> {my_string}  -- {my_string.isalnum()}")
-
 print(f"Length of --> {my_string} -- {len(my_string)}")
-
+print(f"Is following string alpha-numeric --> {my_string}  -- {my_string.isalnum()}")
+print(f"Is following string alpha-numeric --> {my_string}  -- {my_string.isalpha()}")
