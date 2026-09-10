6da1b35cf42174f12becd70d5294e09893d584f9
Vinay Garge <vgarge@gmail.com>
2026-09-11 01:32:10 +0530

    11092026

---
+++ ex/ex1.py
@@ -0,0 +1,10 @@
+name = "Vinay"
+years_of_experience = 4
+automation_experience = 2.5
+is_learning_python = True
+
+
+print(f"{name} is of type {type(name)}")
+print(f"{years_of_experience} is of type {type(years_of_experience)}")
+print(f"{automation_experience} is of type {type(automation_experience)}")
+print(f"{is_learning_python} is of type {type(is_learning_python)}")
---
+++ ex/ex3.py
@@ -0,0 +1,4 @@
+languages = ["Python", "JavaScript", "Java", "TypeScript"]
+
+for language in languages:
+    print(f"{language} is a programming language.")
