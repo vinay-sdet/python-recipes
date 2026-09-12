cd385afaeff89a0582e63c1fc584fb71781cbbd3
Vinay Garge <vgarge@gmail.com>
2026-09-12 10:58:08 +0530

    12092026

--- ex/ex3.py
+++ ex/ex3.py
@@ -2,3 +2,10 @@ languages = ["Python", "JavaScript", "Java", "TypeScript"]
 
 for language in languages:
     print(f"{language} is a programming language.")
+
+print("--------------------")
+
+index = 0
+while index < len(languages):
+    print(f"{languages[index]} is a programming language.")
+    index += 1
