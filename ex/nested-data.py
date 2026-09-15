54b66271b27f21f573b0a73848e354b5bc9202c7
Vinay Garge <vgarge@gmail.com>
2026-09-15 10:37:49 +0530

    15092026

--- ex/nested-data.py
+++ ex/nested-data.py
@@ -5,3 +5,30 @@ employee = {
 }
 
 print("Employee Dictionary - ", employee)
+print("Employee Name - ", employee["name"])
+print("Employee Role - ", employee["role"])
+print("Employee Skills - ", employee["skills"])
+
+for skill in employee["skills"]:
+    print(skill)
+
+print("---- Employee Skill - ", employee["skills"][0])
+
+# Nested Dictionary
+employee = {
+    "name": "Vinay",
+    "contact": {"email": "vinay@example.com", "phone": "1234567890"},
+}
+
+print("Employee Contact-email - ", employee["contact"]["email"])
+
+# Real world example
+
+test_case = {
+    "name": "Login Test",
+    "status": "Failed",
+    "browser": {"name": "Chrome", "version": "140"},
+    "tags": ["login", "smoke", "regression"],
+}
+
+print("Test Case Tags - ", test_case["tags"][1])
