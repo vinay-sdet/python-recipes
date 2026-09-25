3195e0379a356d8a6ce3d336b17f66805238dc9e
Vinay Garge <vgarge@gmail.com>
2026-09-25 23:24:38 +0530

    25092026-2324

--- pandas/test_execution.py
+++ pandas/test_execution.py
@@ -6,3 +6,8 @@ print("First 5 rows\n", df.head(5))
 print("Shape - ", df.shape)
 print("Column Names\n", df.columns.tolist())
 print("Dataframe information\n", df.info())
+
+print("\nMissing values\n", df.isnull().sum())
+print("\nData types\n", df.dtypes)
+print("\nSummary statistics\n", df.describe())
+print("\nLast 5 rows\n", df.tail(5))
